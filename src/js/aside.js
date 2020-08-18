003049af6e61ed0a5023bbe91928a35e7aca4117
Anna Kuzn <annbuho@gmail.com>
2020-08-18 21:00:20 +0300

    1.0.27
    Исправлен js для aside. Удалены контейнеры из модалок, сделана высота на всю страницу. Изменены некоторые ссылки на кнопки.

--- src/js/aside.js
+++ src/js/aside.js
@@ -1,48 +1,42 @@
-const openMenuButtonClickHandler = document.querySelector('.btn-open-burger');
-const modalBtnExit = document.querySelector('.aside__btn-exit');
+const openMenuButton = document.querySelector('.btn-open-burger');
+const closeMenuButton = document.querySelector('.aside__btn-exit');
 const overlay = document.querySelector('.overlay');
 const asideLinks = document.querySelectorAll('.aside__nav-link');
 const aside = document.querySelector('.aside');
-const body = document.querySelector('body');
-const escapeCode = 27;
+const ESCAPE_KEYCODE = 27;
 
 
-let closeMenu = function() {
-    aside.classList.remove('modal--active');
+const closeMenu = function() {
+    aside.classList.remove('aside--active');
     overlay.classList.remove('overlay--active');
-    body.classList.remove('hidden');
 }
 
-let closeMenuByPressBtnExit = function() {
-    modalBtnExit.addEventListener('click', closeMenu);
+const closeMenuButtonClickHandler = function() {
+     closeMenu();
 }
 
-let closeMenuByPressOverlay = function() {
+const closeMenuOverlayClickHandler = function() {
     overlay.addEventListener('click', closeMenu);
 }
 
-let closeMenuByPressEscape = function() {
-    window.addEventListener('keydown', function(btn) {
-        if (btn.keyCode === escapeCode) {
-            closeMenu();
-        }
-    });
+const escapeKeyDownHandler = function(btn) {
+    if (btn.keyCode === ESCAPE_KEYCODE) {
+        closeMenu();
+    }
 }
 
-let closeMenuByLink = function() {
-    asideLinks.forEach(asideLink => 
-        asideLink.addEventListener('click', closeMenu));
+const closeMenuLinkClickHandler = function() {
+    closeMenu();
 }
 
-openMenuButtonClickHandler.addEventListener('click', function() {
-    aside.classList.add('modal--active');
+openMenuButton.addEventListener('click', function() {
+    aside.classList.add('aside--active');
     overlay.classList.add('overlay--active');
-    body.classList.add('hidden');
+});
 
-    closeMenuByPressBtnExit();
-    closeMenuByPressOverlay();
-    closeMenuByPressEscape();
-    closeMenuByLink();
-})
-
-
+closeMenuButton.addEventListener('click', closeMenuButtonClickHandler);
+overlay.addEventListener('click', closeMenuOverlayClickHandler);
+document.addEventListener('keydown', escapeKeyDownHandler);
+asideLinks.forEach(asideLink => 
+    asideLink.addEventListener('click', closeMenuLinkClickHandler)
+);
--- src/js/btn-read-more.js
+++ src/js/btn-read-more.js
@@ -1,14 +1,17 @@
 const btnReadMore = document.querySelectorAll('.section__btn-read-more');
 
+const btnReadMoreClickHandler = function () {
+    let previous = this.previousElementSibling;
+
+    if (!this.hasAttribute('data-section')) {
+        this.setAttribute('data-section', 'open');
+        previous.setAttribute('data-section', 'open');
+    } else {
+        this.removeAttribute('data-section');
+        previous.removeAttribute('data-section');
+    }
+}
+
 btnReadMore.forEach(btn =>
-    btn.addEventListener('click', function () {
-        let previous = btn.previousElementSibling;
-        if (!this.hasAttribute('data-section')) {
-            this.setAttribute('data-section', 'open');
-            previous.setAttribute('data-section', 'open');
-        } else {
-            this.removeAttribute('data-section');
-            previous.removeAttribute('data-section');
-        }
-    })
+    btn.addEventListener('click', btnReadMoreClickHandler)
 );
